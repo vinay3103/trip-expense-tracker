eeb632dcc970bb636a4fa16251026407360c531c
Jadhav Vinay <vinayjadhav1234567@gmail.com>
2026-02-04 22:18:39 +0530

    Add serverless function to get members from Supabase

---
+++ netlify/functions/getMembers.js
@@ -0,0 +1,6 @@
+const { createClient } = require("@supabase/supabase-js");
+const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY);
+exports.handler=async()=>{
+const {data}=await supabase.from("members").select("*");
+return{statusCode:200,body:JSON.stringify(data)};
+};
