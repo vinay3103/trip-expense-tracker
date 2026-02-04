1be3bbc024b403b40cba01a15ff93c0b2bb63cd7
Jadhav Vinay <vinayjadhav1234567@gmail.com>
2026-02-04 22:18:05 +0530

    Add saveMembers function to handle member data

---
+++ netlify/functions/saveMembers.js
@@ -0,0 +1,7 @@
+const { createClient } = require("@supabase/supabase-js");
+const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY);
+exports.handler=async(e)=>{
+const data=JSON.parse(e.body);
+await supabase.from("members").insert([data]);
+return{statusCode:200,body:"ok"};
+};
