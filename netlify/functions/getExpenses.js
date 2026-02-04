f11e64becfdb74f0f109985435b7d05bffd7b4a1
Jadhav Vinay <vinayjadhav1234567@gmail.com>
2026-02-04 22:19:55 +0530

    Refactor getExpenses function to simplify response
    Removed error handling from getExpenses function.

--- netlify/functions/getExpenses.js
+++ netlify/functions/getExpenses.js
@@ -1,12 +1,6 @@
 const { createClient } = require("@supabase/supabase-js");
-
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
-);
-
-exports.handler = async function () {
-  const { data, error } = await supabase.from("expenses").select("*");
-  if (error) return { statusCode:500, body:JSON.stringify(error) };
-  return { statusCode:200, body:JSON.stringify(data) };
+const supabase = createClient(process.env.SUPABASE_URL,process.env.SUPABASE_KEY);
+exports.handler=async()=>{
+const {data}=await supabase.from("expenses").select("*");
+return{statusCode:200,body:JSON.stringify(data)};
 };
