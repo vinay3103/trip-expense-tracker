d086173c492f0aee29924af8e85b9ca2a296cb47
Jadhav Vinay <vinayjadhav1234567@gmail.com>
2026-02-11 15:03:44 +0530

    Update summary.js

--- summary.js
+++ summary.js
@@ -1,6 +1,5 @@
 const SUPABASE_URL = "https://hnvlnezbisyxmrubuarj.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhudmxuZXpiaXN5eG1ydWJ1YXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMTExODAsImV4cCI6MjA4NTc4NzE4MH0.O7B21Nx762JCvx5caCJs3hiE4RO8GX2a9v3LP3Q9x58";
-
 const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let tripId = localStorage.getItem("tripId");
@@ -33,11 +32,11 @@ async function loadSummary(){
     let diff=paid[m.user_id]-avg;
     summaryBody.innerHTML+=`
     <tr>
-      <td>${m.name}</td>
+      <td>${m.email}</td>
       <td class="${diff<0?'red':'green'}">
         ${diff<0
-          ? `➖ Owes ${Math.abs(diff).toFixed(2)}`
-          : `➕ Gets ${diff.toFixed(2)}`
+          ? `Owes ${Math.abs(diff).toFixed(2)}`
+          : `Gets ${diff.toFixed(2)}`
         }
       </td>
     </tr>`;
@@ -45,3 +44,4 @@ async function loadSummary(){
 }
 
 loadSummary();
+
