d52584bc677a21a4057c3b8fb6c5c9517cc397a7
Jadhav Vinay <vinayjadhav1234567@gmail.com>
2026-02-11 13:24:23 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,23 @@
+const SUPABASE_URL = "https://hnvlnezbisyxmrubuarj.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhudmxuZXpiaXN5eG1ydWJ1YXJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMTExODAsImV4cCI6MjA4NTc4NzE4MH0.O7B21Nx762JCvx5caCJs3hiE4RO8GX2a9v3LP3Q9x58";
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+async function login(){
+  const { error } = await db.auth.signInWithPassword({
+    email: email.value,
+    password: password.value
+  });
+
+  if(error) alert(error.message);
+  else window.location.href="index.html";
+}
+
+async function signup(){
+  const { error } = await db.auth.signUp({
+    email: email.value,
+    password: password.value
+  });
+
+  if(error) alert(error.message);
+  else alert("Check email to confirm account.");
+}
